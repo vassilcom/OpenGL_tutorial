eb12b374223000b39130cefd86b5b7cb8ff5cbbd
Vasili <vassilcom@gmail.com>
2018-12-23 19:32:05 +0100

    Vertex Arrays in OpenGL (video 12)

--- OpenGL_tutorial/Main.cpp
+++ OpenGL_tutorial/Main.cpp
@@ -84,6 +84,10 @@ int main(void)
 	if (!glfwInit())
 		return -1;
 
+	//adding core proile without default Vertex Arrays
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	/* Create a windowed mode window and its OpenGL context */
 	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
@@ -103,6 +107,7 @@ int main(void)
 	else 
 		std::cout << glGetString(GL_VERSION) << std::endl;
 
+	
 	float positions[] =
 	{
 		-0.5f, -0.5f,
@@ -115,26 +120,40 @@ int main(void)
 		0,1,2,
 		2,3,0
 	};
+	unsigned int vao;
+	GLCall(glGenVertexArrays(1, &vao));
+	GLCall(glBindVertexArray(vao));
 	
 	unsigned int buffer;//id for buffer
 	GLCall(glGenBuffers(1, &buffer));//assigns id
+	//bind vertex buffer
 	GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));//selects buffer
 	GLCall(glBufferData(GL_ARRAY_BUFFER, (sizeof(positions) / sizeof(*positions)) * sizeof(float), positions, GL_STATIC_DRAW));//set size f buffer, static or dinamic, and give a data, that is oprional (can be NULL, and data can be assigned later)
 
 	GLCall(glEnableVertexAttribArray(0));
+	//setup vertex layout
 	GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0));//aattribute, how many variables, data type, no normalize, size of one vertex, offset where attribute starts 
+	
 	
 	unsigned int ibo;
 	GLCall(glGenBuffers(1, &ibo));
+	//bind index buffer 
 	GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
 	GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, (sizeof(indices) / sizeof(*indices)) * sizeof(unsigned int), indices, GL_STATIC_DRAW));
 
-
+	
 	GLCall(unsigned int shader = CreateShader(StringFromFile("res/vertexShader.shader"), StringFromFile("res/fragmentShader.shader")));
+	//bind shader
 	GLCall(glUseProgram(shader));
 
 	int location = glGetUniformLocation(shader, "u_Color");
 	
+	//temporary unbinding
+	GLCall(glUseProgram(0));//unbind shader
+	GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));//unbind index buffer 
+	GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));//unbind vertex buffer
+	GLCall(glBindVertexArray(0));//unbind vertex Array
+
 	/* Loop until the user closes the window */
 	float n = 0.0f;
 	while (!glfwWindowShouldClose(window))
@@ -145,9 +164,12 @@ int main(void)
 		n =n + 0.01f;
 		float animNormalizedFloat = (float)((int)(n*100.0f) % 100) / 100.0f;
 
+		GLCall(glUseProgram(shader));
 		glUniform4f(location, animNormalizedFloat, .0f, .0f, 1.0f);
-		
-	
+		GLCall(glBindVertexArray(vao));
+		GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
+
+		//draw call 	
 		//since we dont have index buffer yet:
 		GLCall(glDrawElements(GL_TRIANGLES,6,GL_UNSIGNED_INT, nullptr));//type, number of indexes, data type, pointer to index buffer
 
